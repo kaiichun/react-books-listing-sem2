cd8b2d929254de1ea73e357c8eafa218619fcb3e
Lee Kai Chun <kaiichun710@gmail.com>
2023-09-11 00:19:47 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useMemo } from "react";
+import { bookData } from "./data/books";
+
+const BookList = () => {
+  const [books, setBooks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const categories = useMemo(() => {
+    let options = [];
+    /* Instruction: Get all categories from books */
+    return options;
+  }, [books]);
+
+  useEffect(() => {
+    /* instruction: load books from the books data */
+    setBooks(bookData);
+  }, []);
+
+  useEffect(() => {
+    /* Instruction: filter books by selectedCategory */
+    /* Instruction: set filtered books to books state */
+    /* Instruction: set books to all books if selectedCategory is empty */
+  }, [selectedCategory]);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <select onChange={(e) => setSelectedCategory(e.target.value)}>
+            <option value="">All Categories</option>
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      {/* instruction: list books using .map() */}
+      <div className="row">
+        {books.map((book) => (
+          <div className="col-4 my-5" key={book.title}>
+            <div className="card">
+              <img
+                src="../images/book.jpg"
+                alt={book.title}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h2 className="card-text">{book.title}</h2>
+                <p className="card-text">Author: {book.author}</p>
+                <p className="card-text">
+                  <span>
+                    Category:
+                    {book.categories.length > 0 &&
+                      ` ${book.categories.join(", ")}`}
+                  </span>
+                </p>
+                <p className="card-text">Year: {book.year}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BookList;
---
+++ src/data/books.js
@@ -0,0 +1,142 @@
+export const bookData = [
+  {
+    title: "1984",
+    image: "book.jpg",
+    categories: ["Dystopian", "Political"],
+    author: "George Orwell",
+    year: 1949
+  },
+  {
+    title: "To Kill a Mockingbird",
+    image: "book.jpg",
+    categories: ["Classic", "Historical"],
+    author: "Harper Lee",
+    year: 1960
+  },
+  {
+    title: "Brave New World",
+    image: "book.jpg",
+    categories: ["Dystopian", "Science Fiction"],
+    author: "Aldous Huxley",
+    year: 1932
+  },
+  {
+    title: "The Catcher in the Rye",
+    image: "book.jpg",
+    categories: ["Classic", "Young Adult"],
+    author: "J.D. Salinger",
+    year: 1951
+  },
+  {
+    title: "Animal Farm",
+    image: "book.jpg",
+    categories: ["Political", "Satire"],
+    author: "George Orwell",
+    year: 1945
+  },
+  {
+    title: "Fahrenheit 451",
+    image: "book.jpg",
+    categories: ["Dystopian", "Science Fiction"],
+    author: "Ray Bradbury",
+    year: 1953
+  },
+  {
+    title: "Lord of the Flies",
+    image: "book.jpg",
+    categories: ["Classic", "Adventure"],
+    author: "William Golding",
+    year: 1954
+  },
+  {
+    title: "Moby Dick",
+    image: "book.jpg",
+    categories: ["Classic", "Adventure"],
+    author: "Herman Melville",
+    year: 1851
+  },
+  {
+    title: "Pride and Prejudice",
+    image: "book.jpg",
+    categories: ["Classic", "Romance"],
+    author: "Jane Austen",
+    year: 1813
+  },
+  {
+    title: "The Great Gatsby",
+    image: "book.jpg",
+    categories: ["Classic", "Tragedy"],
+    author: "F. Scott Fitzgerald",
+    year: 1925
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    image: "book.jpg",
+    categories: ["Fantasy", "Young Adult"],
+    author: "J.K. Rowling",
+    year: 1997
+  },
+  {
+    title: "The Hobbit",
+    image: "book.jpg",
+    categories: ["Fantasy", "Adventure"],
+    author: "J.R.R. Tolkien",
+    year: 1937
+  },
+  {
+    title: "The Da Vinci Code",
+    image: "book.jpg",
+    categories: ["Mystery", "Thriller"],
+    author: "Dan Brown",
+    year: 2003
+  },
+  {
+    title: "The Kite Runner",
+    image: "book.jpg",
+    categories: ["Historical", "Drama"],
+    author: "Khaled Hosseini",
+    year: 2003
+  },
+  {
+    title: "Life of Pi",
+    image: "book.jpg",
+    categories: ["Adventure", "Fantasy"],
+    author: "Yann Martel",
+    year: 2001
+  },
+  {
+    title: "The Book Thief",
+    image: "book.jpg",
+    categories: ["Historical", "Young Adult"],
+    author: "Markus Zusak",
+    year: 2005
+  },
+  {
+    title: "Gone Girl",
+    image: "book.jpg",
+    categories: ["Mystery", "Thriller"],
+    author: "Gillian Flynn",
+    year: 2012
+  },
+  {
+    title: "The Hunger Games",
+    image: "book.jpg",
+    categories: ["Dystopian", "Young Adult"],
+    author: "Suzanne Collins",
+    year: 2008
+  },
+  {
+    title: "The Alchemist",
+    image: "book.jpg",
+    categories: ["Fantasy", "Adventure"],
+    author: "Paulo Coelho",
+    year: 1988
+  },
+  {
+    title: "Sapiens",
+    image: "book.jpg",
+    categories: ["Non-Fiction", "History"],
+    author: "Yuval Noah Harari",
+    year: 2011
+  }
+];
